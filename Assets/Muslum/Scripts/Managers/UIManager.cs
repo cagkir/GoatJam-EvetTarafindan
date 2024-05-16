980136c509937c00659378f81590a9ed8968a351
cagkir <169288544+cagkir@users.noreply.github.com>
2024-05-16 22:42:50 +0300

    FixLater

---
+++ Assets/Muslum/Scripts/Managers/UIManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI interactableText;
+    [SerializeField] public TextMeshProUGUI keyCounter;
+} 
